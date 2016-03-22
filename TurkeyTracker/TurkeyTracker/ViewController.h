2d44499b5cfb86783a6c06e89e2e135d1aa44248
Branton Boehm <branton@outlook.com>
2016-03-22 15:17:25 -0700

    Add base project

---
+++ TurkeyTracker/TurkeyTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TurkeyTracker
+//
+//  Created by Branton Boehm on 3/22/16.
+//  Copyright © 2016 Fastfoot Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
